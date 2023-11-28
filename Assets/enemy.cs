bd8ed557a8ae712eb78df3e8b109cf2ec5e4b7bf
StsMet <metelskysts@gmail.com>
2023-11-28 13:30:51 +0300

    working enemies

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -21,7 +21,28 @@ public class enemy : MonoBehaviour
 
     private void Update()
     {
-
+        if (movingLeft)
+        {
+            if (transform.position.x > leftEdge)
+            {
+                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                movingLeft = false;
+            }
+        }
+        else
+        {
+            if (transform.position.x < rightEdge)
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                movingLeft = true;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
