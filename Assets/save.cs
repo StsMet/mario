a4baf7e7f556d61fb704772bdc31643114bf2fdf
StsMet <metelskysts@gmail.com>
2024-01-11 21:44:18 +0300

    save

---
+++ Assets/save.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class save : MonoBehaviour
+{
+    public Transform PlayerPosition;
+    public float X, Y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Load();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        X = PlayerPosition.position.x;
+        Y = PlayerPosition.position.y;
+        Save();
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("Save [X]", X);
+        PlayerPrefs.SetFloat("Save [Y]", Y);
+    }
+    private void Load()
+    {
+        if (PlayerPrefs.HasKey("Save [X]"))
+        {
+            X = PlayerPrefs.GetFloat("Save [X]");
+        }
+        if (PlayerPrefs.HasKey("Save [Y]"))
+        {
+            Y = PlayerPrefs.GetFloat("Save [Y]");
+        }
+        PlayerPosition.transform.position = new Vector3(X, Y);
+    }
+}
